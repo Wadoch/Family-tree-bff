a25567ede8e8d8e1130e906c4503258abd72eece
Wadoch <dwadoch@gmail.com>
2018-09-16 12:21:50 +0200

    Setup heroin

---
+++ infrastructure/config.js
@@ -0,0 +1,19 @@
+const heroin = require('heroin-js');
+
+const configurator = heroin(process.env.HEROKU_API_TOKEN);
+
+const copyEnvs = [
+    'MONGODB_DATABASE',
+];
+
+const baseEnvs = copyEnvs.reduce((all, current) =>
+    ({ ...all, [current]: process.env[current] }), {});
+
+const config = {
+    name: 'ps-family-tree-bff',
+    config_vars: {
+        ...baseEnvs,
+    },
+};
+
+configurator(config);
