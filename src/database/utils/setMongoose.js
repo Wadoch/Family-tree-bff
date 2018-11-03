402e756fec3f94359ce1fb3c6157ec229b2dbafc
Wadoch <dwadoch@gmail.com>
2018-11-03 14:11:01 +0100

    Add info about database connection

--- src/database/utils/setMongoose.js
+++ src/database/utils/setMongoose.js
@@ -5,6 +5,6 @@ mongoose.set('useCreateIndex', true);
 mongoose.connect(config.get('mongodb.url'), {useNewUrlParser: true})
     .catch((err) => console.log(err));
 
-mongoose.connection.once('open', () => console.log('Successfully connected to database'));
+mongoose.connection.once('open', () => console.log(`Successfully connected to database: ${config.get('mongodb.url')}`));
 
 module.exports = mongoose;
