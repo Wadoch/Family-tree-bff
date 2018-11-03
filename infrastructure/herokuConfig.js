d27eb395572849d0f7eee00501d676371a35f74c
Wadoch <dwadoch@gmail.com>
2018-11-03 15:18:43 +0100

    Add missing envs

--- infrastructure/herokuConfig.js
+++ infrastructure/herokuConfig.js
@@ -5,6 +5,9 @@ const configurator = heroin(process.env.HEROKU_API_TOKEN);
 const copyEnvs = [
     'MONGODB_DATABASE',
     'JWT_SECRET_KEY',
+    'DECRYPT_USER_DATA_KEY',
+    'DECRYPT_USER_DATA_IV',
+    'DECRYPT_USER_DATA_SALT',
 ];
 
 const baseEnvs = copyEnvs.reduce((all, current) =>
